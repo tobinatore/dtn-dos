92ef0281ae548cd1b1408f3768becddd91428df7
Tobias Nöthlich <noethlich-tobias@t-online.de>
2020-11-23 22:17:21 +0100

    modified teardown script to completely remove all nodes, configs and network namespaces

--- teardown.sh
+++ teardown.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
+echo "How many nodes does your DTN have?"
+read NUM_NODES
+for ((node=1;node<=$NUM_NODES;node++));
+do
+echo -e "\e[31mDeleting network namespace 'nns-dtn-$node'..."
+ip netns delete nns-dtn-$node
+echo -e "\e[31mDeleting directory 'node$node'..."
+rm -r node$node
+done
+echo -e "\e[31mDeleting directory 'nodes'..."
+rm -r nodes
 
-echo -e  "\e[31mDeleting network namespace 'nns-dtn-1'..."
-ip netns delete nns-dtn-1
-echo -e  "\e[31mDeleting network namespace 'nns-dtn-2'..."
-ip netns delete nns-dtn-2
+echo -e "\e[31mDeleting bridge..."
+ip link set br1 down
+sudo ip link del br1
 echo -e  "\e[31mDONE"
 
